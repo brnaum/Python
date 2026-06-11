5fce9fffea634beea0a6d587856ee81550eae2bb
brnaum <brndojob@gmail.com>
2026-06-11 12:12:06 -0300

    exercício 3

---
+++ Atividade3.py
@@ -0,0 +1,12 @@
+# Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um link de Internet (em Mbps), calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos).
+
+arquivo_MB = float(input("Informe o tamanho do arquivo para download (em MB): "))
+velocidade_Mbps = float(input("Informe a velocidade da internet (em Mbps): "))
+#Converter o arquivo de Megabytes para Megabits
+arquivo_Mb = arquivo_MB * 8
+#Calcular o tempo em segundos
+tempo_segundos = arquivo_Mb / velocidade_Mbps
+#Transformar para minutos
+tempo_minutos = tempo_segundos / 60
+
+print(f"O tempo, em minutos, do arquivo informado é de: {tempo_minutos}")
