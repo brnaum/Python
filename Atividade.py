9aaa3e82188af35d0c916ccaf5436a0395579039
brnaum <brndojob@gmail.com>
2026-06-11 11:18:25 -0300

    Lista 1 Nead - Python

---
+++ Atividade.py
@@ -0,0 +1,1 @@
+# O custo ao consumidor de um carro novo é a soma do custo de fábrica com a porcentagem do distribuidor e dos impostos (aplicados ao custo de fábrica). Supondo que a porcentagem do distribuidor seja de 12% do preço de fábrica e os impostos de 30% do preço de fábrica, fazer um programa em Python para ler o custo de fábrica de um carro e imprimir o custo ao consumidor.
---
+++ Atividade2.py
@@ -0,0 +1,7 @@
+# Faça um programa em Python que leia dois números inteiros quaisquer para as variáveis A e B, efetue a troca dos valores de forma que A passe a armazenar o valor de B e que B passe armazenar o valor de A e que imprima os valores trocados.
+
+A = int(input("Escreva um número inteiro qualquer para que ele equivala à A: "))
+B = int(input("Escreva um número inteiro qualquer para que ele equivala à B: "))
+
+print(f"O valor de A é: {B}")
+print(f"O valor de B é: {A}")
