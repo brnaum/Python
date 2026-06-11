147a6eaef1ab71f17eedbcf60095aec204baf48e
brnaum <brndojob@gmail.com>
2026-06-11 12:33:59 -0300

    atividade 4 incompleta

---
+++ Atividade4.py
@@ -0,0 +1,17 @@
+#Elabore um programa em Python que leia as duas notas de prova (P1 e P2) e duas notas de trabalho (T1 e T2) e posteriormente exiba a mensagem ‘Aprovado’ ou ‘Não aprovado’ dependendo dos valores obtidos, conforme as regras de cálculo definidas a seguir:
+#• Média de provas: MP = (P1 + P2)/2
+#• Média de trabalhos: MT = (T1 + T2)/2
+#• Média final: MF = 0,8MP + 0,2MT
+#• Situação:
+#◦ Se MF ≥ 6,0 → aprovado
+#◦ Se MF < 6,0 → não aprovado
+
+p1 = float(input("Informe o valor da nota obtida na prova 1: "))
+p2 = float(input("Informe o valor da nota obtida na prova 2: "))
+t1 = float(input("Informe o valor obtido no trabalho 1: "))
+t2 = float(input("Informe o valor obtido no trabalho 2: "))
+MP = (p1 + p2) / 2
+MT = (t1 + t2) / 2
+MF = (0.8 * MP) + (0.2 * MT)
+
+print(f"O valor da média final é de: {MF}")
